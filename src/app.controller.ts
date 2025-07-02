a370327f87d59c660b857ab4414f5e87dd818a9e
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-07-02 19:01:20 +0900

    TypeORM Theory
    - Relation Options
    - TypeORM Utility
    - Useful Method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import { ILike, LessThan, Repository } from 'typeorm';
 import { ProfileModel } from './entity/profile.entity';
 import { PostModel } from './entity/post.entity';
 import { TagModel } from './entity/tag.entity';
@@ -22,23 +22,152 @@ export class AppController {
   @Get('users')
   getUsers() {
     return this.userRepository.find({
-      relations: {
-        profile: true,
-        posts: true,
+      where: {
+        // 아닌 경우 가져오기
+        // id: Not(1),
+        // 적은 경우 가져오기
+        // 작은 경우 or 같은 경우
+        // id: LessThan(30),
+        // id: LessThanOrEqual(30),
+        // id: MoreThan(30),
+        // id: MoreThanOrEqual(30),
+        // id: Equal(30),
+        // 유사값
+        // email: Like('%google%'),
+        // 대문자 소문자 구분 안하는 유사값
+        // email: ILike('%google%'),
+        // 사이값
+        // id: Between(10, 15),
+        // 해당되는 여러 값
+        // id: In([1, 3, 5, 7, 99]),
       },
+
+      // 어떤 프로퍼티를 선택할지 결정
+      // 기본은 모든 프로퍼티를 가져옴
+      // select 을 정의하면 정의된 프로퍼티들만 가져옴
       // select: {
       //   id: true,
-      //   title: true,
+      //   updatedAt: true,
+      //   createdAt: true,
+      //   version: true,
+      //   profile: {
+      //     id: true,
+      //   },
       // },
+      // 필터링할 조건을 입력
+      // 배열에 넣으면 or 조건으로 동작
+      // 객체 안에 넣으면 and 조건으로 동작
+      // where: [
+      //   {
+      //     version: 2,
+      //   },
+      //   {
+      //     id: 6,
+      //   },
+      //   {
+      //     profile: {
+      //       id: 3,
+      //     },
+      //   },
+      // ],
+      // 관계를 가져오는 법
+      // relations: {
+      //   profile: true,
+      // },
+      // 오름차순, 내림차순
+      // order: {
+      //   id: 'DESC',
+      // },
+      // 처음 몇 개 제외
+      // skip: 1,
+      // 처음 몇 개만 가져옴
+      // take: 1,
     });
   }
 
-  @Post('users')
-  postUser() {
-    return this.userRepository.save({
-      // title: 'test title',
-      // role: Role.ADMIN,
+  @Post('sample')
+  async sample() {
+    // 모델에 해당되는 객체 생성 - 저장은 안함
+    // const user1 = this.userRepository.create({
+    //   email: 'test@wisoft.io',
+    // });
+
+    // 저장
+    // const user2 = await this.userRepository.save({
+    //   email: 'test@wisoft.io',
+    // });
+
+    // preload
+    // 입력된 값을 기반으로 데이터베이스에 있는 데이터를 불러오고
+    // 추가 입력된 값으로 데이터베이스에서 가져온 값들을 대체함.
+    // 저장하지는 않음
+    // const user3 = await this.userRepository.preload({
+    //   id: 101,
+    //   email: 'jaeyoung@wisoft.io',
+    // });
+
+    // 삭제하기
+    // await this.userRepository.delete(101);
+
+    // 값을 증가시킴
+    // await this.userRepository.increment(
+    //   {
+    //     id: 1,
+    //   },
+    //   'count',
+    //   2,
+    // );
+
+    // 값을 감소시킴
+    // await this.userRepository.decrement(
+    //   {
+    //     id: 1,
+    //   },
+    //   'count',
+    //   2,
+    // );
+
+    // 갯수 카운팅하기
+    // const count = await this.userRepository.count({
+    //   where: {
+    //     email: ILike('%0%'),
+    //   },
+    // });
+
+    // 합계
+    // const sum = await this.userRepository.sum('count', {
+    //   email: ILike('%0%'),
+    // });
+
+    // 평균
+    // const avg = await this.userRepository.average('count', {
+    //   id: LessThan(4),
+    // });
+
+    // min
+    // const min = await this.userRepository.minimum('count', {
+    //   id: LessThan(4),
+    // });
+
+    // max
+    // const max = await this.userRepository.maximum('count', {
+    //   id: LessThan(4),
+    // });
+
+    const usersAndCount = await this.userRepository.findAndCount({
+      take: 3,
     });
+
+    return usersAndCount;
+  }
+
+  @Post('users')
+  async postUser() {
+    for (let i = 0; i < 100; i++) {
+      await this.userRepository.save({
+        email: `user-${i}@google.com`,
+      });
+    }
   }
 
   @Patch('users/:id')
@@ -58,19 +187,31 @@ export class AppController {
 
     return this.userRepository.save({
       ...user,
+      email: user.email + '0',
     });
+  }
+
+  @Delete('user/profile/:id')
+  async deleteProfile(
+    @Param('id')
+    id: string,
+  ) {
+    await this.profileRepository.delete(+id);
   }
 
   @Post('user/profile')
   async createUserAndProfile() {
     const user = await this.userRepository.save({
       email: 'jaeyoung@wisoft.io',
+      profile: {
+        profileImg: 'jaeyoung.jpg',
+      },
     });
 
-    await this.profileRepository.save({
-      profileImg: 'jaeyoung.jpg',
-      user,
-    });
+    // await this.profileRepository.save({
+    //   profileImg: 'jaeyoung.jpg',
+    //   user,
+    // });
 
     return user;
   }
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -79,10 +79,31 @@ export class UserModel {
   @Generated('uuid')
   additionalId: number;
 
-  @OneToOne(() => ProfileModel, (profile) => profile.user)
+  @OneToOne(() => ProfileModel, (profile) => profile.user, {
+    // find() 실행 할 때마다 항상 같이 가져올 relation
+    eager: false,
+
+    // 저장할 때 relation 을 한번에 저장 가능
+    cascade: true,
+
+    nullable: true,
+
+    // 관계가 삭제되었을 때
+    // no action -> 아무것도 안함
+    // cascade -> 참조하는 row 도 같이 삭제
+    // set null -> 참조하는 row 에서 참조 id를 null 로 변경
+    // set default -> 기본 세팅으로 설정 (테이블의 기본 세팅)
+    // restrict -> 참조하고 있는 row 가 있는 경우 참조 당하는 row 삭제 불가
+    onDelete: 'CASCADE',
+  })
   @JoinColumn()
   profile: ProfileModel;
 
   @OneToMany(() => PostModel, (post) => post.author)
   posts: PostModel[];
+
+  @Column({
+    default: 0,
+  })
+  count: number;
 }
